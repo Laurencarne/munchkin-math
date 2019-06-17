3144890cfd5995ada9df2ef13ca8c5de00024414
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-06-17 11:13:38 +0100

    before making join table

---
+++ munchkin-math-backend/db/migrate/20190617100418_create_questions.rb
@@ -0,0 +1,13 @@
+class CreateQuestions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :questions do |t|
+      t.integer :number
+      t.text :question
+      t.text :correct_answer
+      t.text :answer1
+      t.text :answer2
+      t.text :answer3
+      t.timestamps
+    end
+  end
+end
