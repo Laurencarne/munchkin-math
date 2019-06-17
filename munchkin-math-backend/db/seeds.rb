886ae95e7c9f4d4031c161fd925464eab08d8194
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-06-17 12:14:38 +0100

    initial test data

--- munchkin-math-backend/db/seeds.rb
+++ munchkin-math-backend/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seeding Users
+
+lauren = User.create(name: "Lauren", color: "Purple" age: 5)
+patrick = User.create(name: "Patrick", color: "Green", age: 4)
+
+# Seeding Test
+addition101 = Test.create(name: "Basic Addition")
+
+# Seeding Questions
+
+AdditionQ1 = Question.create(testId: 1, number: 1, question: "What does 2 + 3 equal?", correct_answer: "5", answer1: "4", answer2: "9", answer3: "1")
+AdditionQ2 = Question.create(testId: 1, number: 2, question: "What does 4 + 4 equal?", correct_answer: "8", answer1: "10", answer2: "5", answer3: "12")
