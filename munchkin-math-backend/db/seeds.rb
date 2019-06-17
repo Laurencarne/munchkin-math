ec67570cacee283c14f110fbc0705a9ad9c364ad
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-06-17 16:52:09 +0100

    more seed data, UserTest loop untested

--- munchkin-math-backend/db/seeds.rb
+++ munchkin-math-backend/db/seeds.rb
@@ -15,7 +15,9 @@ end
 
 
 # Seeding Test
-addition101 = Test.create(name: "Basic Addition")
+additionTest = Test.create(name: "Basic Addition")
+subtractionTest = Test.create(name: "Basic Subtraction")
+
 
 # Seeding Questions
 
@@ -27,7 +29,15 @@ additionQ5 = Question.create(test_id: 1, number: 5, question: "What does 3 + 4 e
 additionQ6 = Question.create(test_id: 1, number: 6, question: "What does 8 + 9 equal?", correct_answer: "17", answer1: "13", answer2: "11", answer3: "4")
 additionQ7 = Question.create(test_id: 1, number: 7, question: "What does 2 + 11 equal?", correct_answer: "13", answer1: "5", answer2: "9", answer3: "12")
 
+subtractionQ1 = Question.create(test_id: 2, number: 8, question: "What does 12 - 3 equal?", correct_answer: "9", answer1: "4", answer2: "15", answer3: "1")
+subtractionQ2 = Question.create(test_id: 2, number: 9, question: "What does 4 - 4 equal?", correct_answer: "0", answer1: "2", answer2: "8", answer3: "3")
+subtractionQ3 = Question.create(test_id: 2, number: 10, question: "What does 12 - 7 equal?", correct_answer: "5", answer1: "10", answer2: "3", answer3: "8")
+subtractionQ4 = Question.create(test_id: 2, number: 11, question: "What does 8 - 6 equal?", correct_answer: "2", answer1: "9", answer2: "17", answer3: "6")
+subtractionQ5 = Question.create(test_id: 2, number: 12, question: "What does 4 - 3 equal?", correct_answer: "1", answer1: "7", answer2: "12", answer3: "4")
+subtractionQ6 = Question.create(test_id: 2, number: 13, question: "What does 18 - 6 equal?", correct_answer: "12", answer1: "9", answer2: "10", answer3: "15")
+subtractionQ7 = Question.create(test_id: 2, number: 14, question: "What does 5 - 2 equal?", correct_answer: "3", answer1: "5", answer2: "9", answer3: "7")
+
 
 # Seeding UserTests
 
-usertest1 =  UserTest.create(test_id: 1, user_id: 1, score: 50)
+20.times do  UserTest.create(test_id: Test.all.sample.id, user_id: User.all.sample.id, score: 0)
