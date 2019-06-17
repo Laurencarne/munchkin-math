20c1d88c08da5f0d076231aa16bae34428fb54b5
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-17 09:10:13 +0100

    Front page done

--- munchkin-math-frontend/script.js
+++ munchkin-math-frontend/script.js
@@ -1,30 +1,33 @@
 const form = document.querySelector("form");
 const balloonDiv = document.querySelector(".balloonDiv");
-
+///////////////////////
 form.addEventListener("submit", sayHello);
 form.addEventListener("submit", flyBalloon);
-
+form.addEventListener("submit", enterSite);
+///////////////////////
 function sayHello() {
   event.preventDefault();
 
-  const nameH3 = document.createElement("h3");
-  nameH3.innerText = ` Hello ${event.target[0].value}`;
-  nameH3.className = "bounce";
-
   const nameDiv = document.querySelector(".nameBounce");
-  nameDiv.appendChild(nameH3);
 
-  console.log(event.target[0].value);
+  let splitName = event.target[0].value.split("");
+
+  splitName.forEach(letter => {
+    const span = document.createElement("span");
+    span.innerText = letter;
+    span.className = "letterBounce";
+    nameDiv.appendChild(span);
+  });
 
   event.target.reset();
 }
-
+///////////////////////
 function flyBalloon() {
   form.style.display = "none";
 
-  setInterval(function() {
+  let int1 = setInterval(function() {
     const img1 = document.createElement("img");
-    img1.src = "http://clipart-library.com/data_images/19399.png";
+    img1.src = "https://media-public.canva.com/MADZbPBp-pU/1/screen.svg";
     img1.className = "balloon1";
     balloonDiv.append(img1);
     setTimeout(() => {
@@ -32,10 +35,9 @@ function flyBalloon() {
     }, 4300);
   }, 1000);
 
-  setInterval(() => {
+  let int2 = setInterval(() => {
     const img2 = document.createElement("img");
-    img2.src =
-      "https://i1.wp.com/freepngimages.com/wp-content/uploads/2016/04/yellow-balloon-transparent-background.png?fit=491%2C752";
+    img2.src = "https://media-public.canva.com/MADZbNX3BTU/1/screen.svg";
     img2.className = "balloon2";
     balloonDiv.append(img2);
     setTimeout(() => {
@@ -43,14 +45,80 @@ function flyBalloon() {
     }, 4300);
   }, 1300);
 
-  setInterval(() => {
+  let int3 = setInterval(() => {
     const img3 = document.createElement("img");
-    img3.src =
-      "https://i2.wp.com/freepngimages.com/wp-content/uploads/2016/04/red-balloon-transparent-background.png?fit=491%2C752";
+    img3.src = "https://media-public.canva.com/MADZbOXV3RI/1/screen.svg";
     img3.className = "balloon3";
     balloonDiv.append(img3);
     setTimeout(() => {
       balloonDiv.removeChild(img3);
     }, 4300);
   }, 1700);
+
+  let int4 = setInterval(() => {
+    const img4 = document.createElement("img");
+    img4.src = "https://media-public.canva.com/MADZbI2uSjA/1/screen.svg";
+    img4.className = "balloon4";
+    balloonDiv.append(img4);
+    setTimeout(() => {
+      balloonDiv.removeChild(img4);
+    }, 4300);
+  }, 1300);
+
+  let int5 = setInterval(() => {
+    const img5 = document.createElement("img");
+    img5.src = "https://media-public.canva.com/MADZbP_94iI/1/screen.svg";
+    img5.className = "balloon5";
+    balloonDiv.append(img5);
+    setTimeout(() => {
+      balloonDiv.removeChild(img5);
+    }, 4300);
+  }, 1000);
+
+  let int6 = setInterval(() => {
+    const img6 = document.createElement("img");
+    img6.src = "https://media-public.canva.com/MADZbBBEn0M/1/screen.svg";
+    img6.className = "balloon6";
+    balloonDiv.append(img6);
+    setTimeout(() => {
+      balloonDiv.removeChild(img6);
+    }, 4300);
+  }, 1300);
+
+  setInterval(() => {
+    clearInterval(int1);
+  }, 5000);
+
+  setInterval(() => {
+    clearInterval(int2);
+  }, 6000);
+
+  setInterval(() => {
+    clearInterval(int3);
+  }, 6500);
+
+  setInterval(() => {
+    clearInterval(int4);
+  }, 7000);
+
+  setInterval(() => {
+    clearInterval(int5);
+  }, 8000);
+
+  setInterval(() => {
+    clearInterval(int6);
+  }, 8500);
 }
+///////////////////////
+function enterSite() {
+  setTimeout(() => {
+    const enterSiteDiv = document.querySelector(".enterSite");
+
+    const enterBtn = document.createElement("button");
+    enterBtn.innerText = "Enter Site";
+    enterBtn.className = "enterBtn";
+
+    enterSiteDiv.appendChild(enterBtn);
+  }, 4000);
+}
+///////////////////////
