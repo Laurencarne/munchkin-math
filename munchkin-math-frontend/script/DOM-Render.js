fa9ce68355a1e408632c339dc4d5ee0980fd156f
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-06-18 18:13:45 +0100

    yet to figure out progressing through question set

--- munchkin-math-frontend/script/DOM-Render.js
+++ munchkin-math-frontend/script/DOM-Render.js
@@ -1,22 +1,66 @@
 const baseUrl = "http://localhost:3000/";
-const usersUrl = `${baseUrl}users`;
-const testsUrl = `${baseUrl}tests`;
-const questionsUrl = `${baseUrl}questions`;
-
-const fetchTestsData = () => fetch(testsUrl).then(response => response.json());
-
+const usersUrl = `${baseUrl}users/`;
+const testsUrl = `${baseUrl}tests/`;
+const questionsUrl = `${baseUrl}questions/`;
 const getJsonData = json => json;
-//
-// // function renderQuestions(json) {
-// //   // return data.find(data => data.name === testName);
-// //   json.forEach(test => {
-// //     test.questions.forEach(question => {
-// //       const pp = document.createElement("p");
-// //       pp.innerText = question.question;
-// //       body.appendChild(pp);
-// //     });
-// //   });
-// // }
+
+let questionCounter = 0;
+
+const GetAllTestsFromServer = () =>
+  fetch(testsUrl).then(response => response.json());
+
+function getSingleTestFromServer(testId) {
+  return fetch(testsUrl + `${testId}`).then(response => response.json());
+}
+
+function runTest(testId) {
+  getSingleTestFromServer(testId).then(renderQuestion);
+}
+
+function onNextBtnClick() {
+  event.preventDefault();
+  debugger;
+  questionCounter++;
+  renderQuestion();
+}
+
+function renderQuestion(testObject) {
+  body.innerHTML = "";
+
+  const questionObject = testObject.questions[questionCounter];
+
+  const questionCard = document.createElement("div");
+
+  const questionDiv = document.createElement("div");
+  questionDiv.innerHTML = `<h3> ${questionObject.question} </h3> <img src= ${
+    questionObject.image_url
+  }>`;
+
+  const answersDiv = document.createElement("div");
+
+  const answerA = document.createElement("div");
+  answerA.innerText = questionObject.correct_answer;
+
+  const answerB = document.createElement("div");
+  answerB.innerText = questionObject.answer1;
+
+  const answerC = document.createElement("div");
+  answerC.innerText = questionObject.answer2;
+
+  const answerD = document.createElement("div");
+  answerD.innerText = questionObject.answer3;
+
+  answersDiv.append(answerA, answerB, answerC, answerD);
+
+  const nextBtn = document.createElement("button");
+  nextBtn.innerText = "Next Question";
+  nextBtn.addEventListener("click", onNextBtnClick);
+
+  questionCard.append(questionDiv, answersDiv, nextBtn);
+
+  body.append(questionCard);
+}
+
 //
 // function fetchThenRenderTestQuestion() {
 //   return fetchTestsData()
