1edb075f35bf684dba3473592072a838f0bfe3e8
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-18 11:34:57 +0100

    Added URL avatar to User

---
+++ munchkin-math-backend/db/migrate/20190618094618_add_collumn_to_users.rb
@@ -0,0 +1,5 @@
+class AddCollumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :avatar, :string
+  end
+end
--- munchkin-math-frontend/script/DOM-Users-Login.js
+++ munchkin-math-frontend/script/DOM-Users-Login.js
@@ -1,4 +1,39 @@
+const fetchUsers = () => fetch(usersUrl).then(response => response.json());
+
+// const getJsonData = json => json;
+
 function loginPage() {
-  // body.html = "";
-  console.log("Log In");
+  body.html = "";
+}
+
+function makeUserCards(user) {
+  const flexDiv = document.createElement("div");
+  flexDiv.className = flexDiv;
+
+  const h3 = document.createElement("h3");
+  h3.className = "loginName";
+  h3.innerText = user.name;
+
+  const p = document.createElement("p");
+  p.className = "loginAge";
+  p.innerText = `Age: ${user.age}`;
+
+  const img = document.createElement("img");
+  img.className = "userAvatarImg";
+  img.src = user.avatar;
+
+  flexDiv.append(h3, img, p);
+
+  return flexDiv;
+}
+
+function renderUsers(json) {
+  body.innerHTML = "";
+  json.forEach(function(user) {
+    body.appendChild(makeUserCards(user));
+  });
+}
+
+function fetchThenRenderUsers() {
+  fetchUsers().then(renderUsers);
 }
