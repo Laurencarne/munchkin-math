16ffd89fb8955c0106c2f7ebcc21ecbc6e39e920
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-20 17:48:26 +0100

    Chnaged login images

--- munchkin-math-frontend/script/DOM-WelcomePage.js
+++ munchkin-math-frontend/script/DOM-WelcomePage.js
@@ -2,29 +2,29 @@ document.addEventListener("DOMContentLoaded", function() {
   loginOrSignUp();
 });
 
-function loginOrSignUp() {
-  resetPage();
-  createHeader("Welcome Munchkin");
-
-  const loginDiv = createElement("div", "innerDiv");
-
-  loginDiv.append(
-    createImage("https://i.imgur.com/mXLrWxX.png", "loginImg"),
-    createElement("p", "pLoginOrSignup", "Login")
-  );
-
-  const signupDiv = createElement("div", "innerDiv");
-
-  signupDiv.append(
-    createImage("https://i.imgur.com/WGWvq0q.png", "loginImg"),
-    createElement("p", "pLoginOrSignup", "Sign Up")
-  );
-
-  flexDivBody.append(loginDiv, signupDiv);
-
-  loginDiv.addEventListener("click", fetchThenRenderUsers);
-  signupDiv.addEventListener("click", signupPage);
-}
+// function loginOrSignUp() {
+//   resetPage();
+//   createHeader("Welcome Munchkin");
+//
+//   const loginDiv = createElement("div", "innerDiv");
+//
+//   loginDiv.append(
+//     createImage("https://i.imgur.com/mXLrWxX.png", "loginImg"),
+//     createElement("p", "pLoginOrSignup", "Login")
+//   );
+//
+//   const signupDiv = createElement("div", "innerDiv");
+//
+//   signupDiv.append(
+//     createImage("https://i.imgur.com/WGWvq0q.png", "loginImg"),
+//     createElement("p", "pLoginOrSignup", "Sign Up")
+//   );
+//
+//   flexDivBody.append(loginDiv, signupDiv);
+//
+//   loginDiv.addEventListener("click", fetchThenRenderUsers);
+//   signupDiv.addEventListener("click", signupPage);
+// }
 
 function sayHello(user) {
   currentPage = "Balloons";
@@ -47,4 +47,20 @@ function sayHello(user) {
   setTimeout(() => {
     landingPage();
   }, 9500);
+}
+
+function loginOrSignUp() {
+  resetPage();
+  createHeader("Welcome Munchkin");
+
+  const loginDiv = createElement("div", "innerDivTest");
+  loginDiv.append(createElement("p", "words", "Login"));
+
+  const signupDiv = createElement("div", "innerDivTest");
+  signupDiv.append(createElement("p", "words", "Sign Up"));
+
+  flexDivBody.append(loginDiv, signupDiv);
+
+  loginDiv.addEventListener("click", fetchThenRenderUsers);
+  signupDiv.addEventListener("click", signupPage);
 }
